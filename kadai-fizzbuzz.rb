b03dfe9ea66725767dd73666432e7307d5a9fcf4
yusuke.nagao <y.n.227@icloud.com>
2021-09-21 09:47:01 +0000

    kadai_ruby_fizzbuzz

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,17 @@
+num_max = 100
+def fizzbuzz(num)
+if num % 15 == 0
+  puts 'FizzBuzz'
+elsif num % 3 == 0
+  puts 'Fizz'
+elsif num % 5 == 0
+  puts 'Buzz'
+else
+  puts num
+end
+end
+(1..num_max).each do |num|
+  fizzbuzz(num)
+end
+
+
